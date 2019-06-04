720f4551a630e04ee55d85fb132b814f98bc9cd9
Mohammed Yassine Chraibi <medyassinechraibi@gmail.com>
2019-06-04 22:51:37 +0000

    Refactored User controller

--- server/app/Http/Controllers/Controller.php
+++ server/app/Http/Controllers/Controller.php
@@ -6,10 +6,22 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Validator;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+
+    /**
+     * User input Validation 
+     */
+    public function validateRequest($request, $rule){
+        $validator = Validator::make($request, $rule);
+
+        if ($validator->fails())
+            return $validator->messages();
+        return null;
+    }
     
 }
